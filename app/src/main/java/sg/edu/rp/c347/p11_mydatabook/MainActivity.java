9ad7fbd216697cfdf15634fb49f1321afae0f83d
18004885 <18004885@myrp.edu.sg>
2020-08-07 14:54:49 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c347/p11_mydatabook/MainActivity.java
@@ -0,0 +1,66 @@
+package sg.edu.rp.c347.p11_mydatabook;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.FrameLayout;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String [] drawerItems;
+    private DrawerLayout drawerLayout;
+    private ListView drawerList;
+    ArrayAdapter<String> aa;
+    String currentTitle;
+    ActionBar ab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        drawerList = findViewById(R.id.left_drawer);
+
+        drawerItems = new String[]{"Bio", "Vaccination", "Anniversary", "About Us"};
+        ab = getSupportActionBar();
+
+        aa = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_activated_1,drawerItems);
+        drawerList.setAdapter(aa);
+
+        drawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> arg0 , View arg1, int position, long arg3) {
+                Fragment fragment = null;
+                if (position == 0){
+                    fragment = new BioFragment();
+                }else if (position == 1){
+                    fragment = new VaccinationFragment();
+                }else if (position == 2){
+                    fragment = new AnniversaryFragment();
+                }else if (position == 3){
+                    fragment = new AboutUsFragment();
+                }
+
+                FragmentManager fm = getSupportFragmentManager();
+                FragmentTransaction transaction = fm.beginTransaction();
+                transaction.replace(R.id.content_frame,fragment);
+                transaction.commit();
+                drawerList.setItemChecked(position,true);
+                currentTitle = drawerItems[position];
+                ab.setTitle(currentTitle);
+                drawerLayout.closeDrawer(drawerList);
+
+            }
+        });
+    }
+}
