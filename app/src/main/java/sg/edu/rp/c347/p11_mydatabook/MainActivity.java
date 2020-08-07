9c49aaf1c5ef35020d22d752fc3ab511af2e4b17
18004885 <18004885@myrp.edu.sg>
2020-08-07 18:43:55 +0800

    Final

--- app/src/main/java/sg/edu/rp/c347/p11_mydatabook/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c347/p11_mydatabook/MainActivity.java
@@ -1,55 +1,84 @@
 package sg.edu.rp.c347.p11_mydatabook;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.FrameLayout;
 import android.widget.ListView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
 
     private String [] drawerItems;
     private DrawerLayout drawerLayout;
+
     private ListView drawerList;
+    private ActionBarDrawerToggle drawerToggle;
     ArrayAdapter<String> aa;
     String currentTitle;
     ActionBar ab;
+    FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        currentTitle = this.getTitle().toString();
         drawerLayout = findViewById(R.id.drawer_layout);
         drawerList = findViewById(R.id.left_drawer);
+        floatingActionButton = findViewById(R.id.fab);
+
+
+        drawerToggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.drawer_open,R.string.drawer_close){
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                ab.setTitle(currentTitle);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                ab.setTitle("P11-My Data Book");
+            }
+        };
+
+
 
         drawerItems = new String[]{"Bio", "Vaccination", "Anniversary", "About Us"};
         ab = getSupportActionBar();
 
-        aa = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_activated_1,drawerItems);
+        aa = new DrawerArrayAdapter(this,R.layout.list,drawerItems);
         drawerList.setAdapter(aa);
 
         drawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> arg0 , View arg1, int position, long arg3) {
                 Fragment fragment = null;
-                if (position == 0){
+                if (position == 0)
                     fragment = new BioFragment();
-                }else if (position == 1){
+                else if (position == 1)
                     fragment = new VaccinationFragment();
-                }else if (position == 2){
+                else if (position == 2)
                     fragment = new AnniversaryFragment();
-                }else if (position == 3){
+                else if (position == 3)
                     fragment = new AboutUsFragment();
-                }
+
 
                 FragmentManager fm = getSupportFragmentManager();
                 FragmentTransaction transaction = fm.beginTransaction();
@@ -60,7 +89,41 @@ public class MainActivity extends AppCompatActivity {
                 ab.setTitle(currentTitle);
                 drawerLayout.closeDrawer(drawerList);
 
+
+
             }
         });
+
+
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.openDrawer(drawerList);
+            }
+        });
+
+
+        drawerLayout.addDrawerListener(drawerToggle);
+        ab.setDisplayHomeAsUpEnabled(true);
     }
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (drawerToggle.onOptionsItemSelected(item))
+            return true;
+        return super.onOptionsItemSelected(item);
+    }
+
 }
